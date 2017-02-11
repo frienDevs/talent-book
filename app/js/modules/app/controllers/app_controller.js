edfe1ba114704b44a5156741d3aad26427e7ed20
prakash vadrevu <prakash.vadrevu@cariboutech.com>
2017-02-12 03:16:32 +0530

    fixing the youtube embed link html tags

--- app/js/modules/app/controllers/app_controller.js
+++ app/js/modules/app/controllers/app_controller.js
@@ -1,5 +1,5 @@
-app_module.controller('AppController', ['$scope', '$state', 'States', 'UserDetailsService', '$window',
-    function($scope, $state, States, UserDetailsService, $window) {
+app_module.controller('AppController', ['$scope', '$state', 'States', 'UserDetailsService', '$window', '$sce',
+    function($scope, $state, States, UserDetailsService, $window, $sce) {
         console.log("app controller");
         var self = this;
         console.log("$state.current.name : " + $state.current.name);
@@ -26,6 +26,49 @@ app_module.controller('AppController', ['$scope', '$state', 'States', 'UserDetai
                 name: "Nagendra Varma"
             }
         ];
+
+        self.posts = [
+            {
+                "user" : {
+                    photoUrl: "resources/dummy/images/puppy.jpg",
+                    name: "Prakash Vadrevu"
+                },
+                uid: "123456",
+                type: "IMAGE",
+                title: "My puppy",
+                desc: "Very nice puppy",
+                url : "resources/dummy/images/puppy.jpg",
+                tags : ["PUPPY", "PHOTOGRAPHY", "DOG"]
+            },
+            {
+                "user" : {
+                    photoUrl: "resources/dummy/images/puppy.jpg",
+                    name: "Prakash Vadrevu"
+                },
+                uid: "123456",
+                type: "VIDEO",
+                title: "300+",
+                desc: "Fantastic batting",
+                url : "https://www.youtube.com/embed/5qgcxWLeu_8",
+                tags : ["CRICKET"]
+            },
+            {
+                "user" : {
+                    photoUrl: "resources/dummy/images/puppy.jpg",
+                    name: "Prakash Vadrevu"
+                },
+                uid: "123456",
+                type: "IMAGE",
+                title: "My puppy 2",
+                desc: "Very nice puppy 2",
+                url : "resources/dummy/images/puppy.jpg",
+                tags : ["PUPPY", "PHOTOGRAPHY", "DOG"]
+            }
+        ];
+
+        self.embeddedUrl = function(url) {
+            return $sce.trustAsResourceUrl(url);
+        };
 
         // if (UserDetailsService.isUserLoggedIn()) {
         // 	if (UserDetailsService.isRootSuperAdmin())
